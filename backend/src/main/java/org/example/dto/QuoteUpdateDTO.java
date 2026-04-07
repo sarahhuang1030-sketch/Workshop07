11a4a4dbb34b55cf36785d4343d3f335cbe297d5
Sherry <yun11261016@gmail.com>
2026-04-07 16:56:50 -0600

    update home page

---
+++ backend/src/main/java/org/example/dto/QuoteUpdateDTO.java
@@ -0,0 +1,13 @@
+package org.example.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class QuoteDTO {
+
+    public Integer customerId;
+
+    public Integer planId;
+
+    public List<Integer> addonIds;
+}
